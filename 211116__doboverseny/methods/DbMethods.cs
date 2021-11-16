dcb5a5ee3b1e878afa97b5054ea7f85fe0bf524c
Peter <mrpeterfulop@gmail.com>
2021-11-16 22:43:21 +0100

    Add project files.

---
+++ 211116__doboverseny/methods/DbMethods.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace _211116__doboverseny.methods
+{
+    class DbMethods
+    {
+
+        public static void getAndFillGrid(DataGrid grid)
+        {
+            using (var db = new DoboszamokEntities())
+            {
+                grid.ItemsSource = db.versenyzok.Include("versenyszamok").ToList();
+                grid.Items.Refresh();
+            }
+        }
+
+        public static void searchAndFillGrid(DataGrid grid, bool rajtszam, string keres)
+        {
+            using (var db = new DoboszamokEntities())
+            {
+
+                if (rajtszam && int.TryParse(keres, out int szam))
+                {
+                    var filtered = db.versenyzok.Include("versenyszamok").Where(x => x.rajtszam == szam).ToList();
+                    grid.ItemsSource = filtered;
+                    grid.Items.Refresh();
+                }
+                else if(!rajtszam && !int.TryParse(keres, out int b))
+                {
+                     var filtered = db.versenyzok.Include("versenyszamok").Where(x => x.nev.Contains(keres)).ToList();
+                     grid.ItemsSource = filtered;
+                     grid.Items.Refresh();
+                }
+                else
+                {
+                    MessageBox.Show("Hibás bementi paraméterek!");
+                }
+            }
+        }
+
+        public static void addNewPerson(string nev, DateTime szuletes, string nem, int versenyszamId, string korosztaly)
+        {
+            using (var db = new DoboszamokEntities())
+            {
+                var uj = new versenyzok
+                {
+                    nev = nev,
+                    szuletes = szuletes,
+                    fk_versenyszam_id = versenyszamId,
+                    korosztaly = korosztaly,
+                    nem = nem
+                };
+                db.versenyzok.Add(uj);
+                db.SaveChanges();
+            }
+        }
+
+        public static void editPerson(int rajtszam, string nev, DateTime szuletes, string nem, int versenyszamId, string korosztaly)
+        {
+            using (var db = new DoboszamokEntities())
+            {
+                var user = db.versenyzok.FirstOrDefault(x => x.rajtszam == rajtszam);
+                user.nev = nev;
+                user.szuletes = szuletes;
+                user.nem = nem;
+                user.fk_versenyszam_id = versenyszamId;
+                user.korosztaly = korosztaly;
+                db.SaveChanges();
+            }
+        }
+
+        public static void deletePerson(int rajtszam)
+        {
+            using (var db = new DoboszamokEntities())
+            {
+                var delete = db.versenyzok.Where(x => x.rajtszam == rajtszam);
+                db.versenyzok.RemoveRange(delete);
+                db.SaveChanges();
+            }
+        }
+
+        public static void exportData(bool versenyzok, string versenyszam)
+        {
+            using (var db = new DoboszamokEntities())
+            {
+
+
+                if (versenyzok)
+                {
+                    Console.WriteLine("Versenyzok export");
+
+                    var export = db.versenyzok.Include("versenyszamok").ToList();
+
+                    
+                    using (var fs = new FileStream($"1_export.csv", FileMode.CreateNew))
+                    {
+                        using (var sw = new StreamWriter(fs,Encoding.UTF8))
+                        {
+                            foreach (var item in export)
+                            {
+                                sw.WriteLine($"{item.rajtszam};{item.nev};{item.szuletes};{item.nem};{item.korosztaly}");
+                            }
+                        }
+                    }
+
+                }
+                else
+                {
+                    Console.WriteLine("Versenyszamok export");
+
+                    var export = db.versenyzok.Include("versenyszamok").Where(x=>x.versenyszamok.nev == versenyszam).ToList();
+
+                    using (var fs = new FileStream($"2_export.csv", FileMode.CreateNew))
+                    {
+                        using (var sw = new StreamWriter(fs, Encoding.UTF8))
+                        {
+                            foreach (var item in export)
+                            {
+                                sw.WriteLine($"{item.versenyszamok.nev};{item.nev}");
+                            }
+                        }
+                    }
+                }
+
+                MessageBox.Show("Exportálás sikeres!");
+
+            }
+        }
+
+
+
+        public static List<versenyszamok> getVersenyszamok()
+        {
+            using (var db = new DoboszamokEntities())
+            {
+                return db.versenyszamok.ToList();
+            }
+        }
+
+        public static int getVersenyszamId(string versenyszam)
+        {
+            using (var db = new DoboszamokEntities())
+            {
+                return Convert.ToInt32(db.versenyszamok.FirstOrDefault(x=>x.nev == versenyszam).Id.ToString());
+            }
+        }
+    }
+}
---
+++ 211116__doboverseny/views/mainView.xaml.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using _211116__doboverseny.methods;
+
+namespace _211116__doboverseny.views
+{
+    /// <summary>
+    /// Interaction logic for mainView.xaml
+    /// </summary>
+    public partial class mainView : Page
+    {
+
+        private int activeUserId { get; set; }
+
+        public mainView()
+        {
+            InitializeComponent();
+            DbMethods.getAndFillGrid(dg_versenyzok);
+            var versenyszamok = DbMethods.getVersenyszamok();
+            setInputs();
+
+        }
+
+
+        private void setInputs()
+        {
+            inp_nem.ItemsSource = new List<string>() { "ferfi", "no" };
+
+            DbMethods.getVersenyszamok().ForEach(x =>
+            {
+                inp_verseny.Items.Add(x.nev);
+                export_options.Items.Add(x.nev);
+            });
+        }
+
+        private void resetInputs()
+        {
+            update_btn.Visibility = Visibility.Hidden;
+            delete_btn.Visibility = Visibility.Hidden;
+            back_btn.Visibility = Visibility.Hidden;
+            add_btn.Visibility = Visibility.Visible;
+
+            inp_name.Clear();
+            inp_kor.Clear();
+            inp_verseny.SelectedIndex = 0;
+            inp_nem.SelectedIndex = 0;
+        }
+
+
+
+        private void search_btn_Click(object sender, RoutedEventArgs e)
+        {
+            var rajtszam = radio_rajtszam.IsChecked.Value;
+            var kereses = inp_search.Text;
+
+            if (!string.IsNullOrWhiteSpace(kereses))
+            {
+                DbMethods.searchAndFillGrid(dg_versenyzok, rajtszam, kereses);
+            }
+            else
+            {
+                MessageBox.Show("Nem adtál meg adatot!");
+                DbMethods.getAndFillGrid(dg_versenyzok);
+            }
+
+        }
+
+        private void export_btn_Click(object sender, RoutedEventArgs e)
+        {
+            DbMethods.exportData(radio_versenyzo.IsChecked.Value, export_options.SelectedItem.ToString());
+
+        }
+
+        private void refresh_btn_Click(object sender, RoutedEventArgs e)
+        {
+            DbMethods.getAndFillGrid(dg_versenyzok);
+        }
+
+        private void dg_versenyzok_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            var row = dg_versenyzok.SelectedItem;
+
+            update_btn.Visibility = Visibility.Visible;
+            delete_btn.Visibility = Visibility.Visible;
+            back_btn.Visibility = Visibility.Visible;
+            add_btn.Visibility = Visibility.Hidden;
+
+            if (!dg_versenyzok.SelectedIndex.Equals(-1))
+            {
+                using (var db = new DoboszamokEntities())
+                {
+                    versenyzok selection = (versenyzok)row;
+
+                    inp_name.Text = selection.nev;
+                    inp_date.SelectedDate = selection.szuletes;
+                    inp_verseny.SelectedItem = selection.versenyszamok.nev;
+                    inp_nem.SelectedItem = selection.nem;
+                    inp_kor.Text = selection.korosztaly;
+                    activeUserId = selection.rajtszam;
+                }
+            }
+
+
+        }
+
+        private void add_btn_Click(object sender, RoutedEventArgs e)
+        {
+            var nev = inp_name.Text;
+            var szuletes = inp_date.SelectedDate;
+            var nem = inp_nem.SelectedItem.ToString();
+            var versenyszamId = DbMethods.getVersenyszamId(inp_verseny.SelectedItem.ToString());
+            var korosztaly = inp_kor.Text;
+
+            if (string.IsNullOrWhiteSpace(nev) || string.IsNullOrWhiteSpace(nem) || string.IsNullOrWhiteSpace(korosztaly) || versenyszamId == 0 || inp_date.SelectedDate is null)
+            {
+                MessageBox.Show("Minden adat kitöltése kötelező!");
+                return;
+            }
+
+            DbMethods.addNewPerson(nev, szuletes.Value, nem, versenyszamId, korosztaly);
+            MessageBox.Show("A bevitel sikeres volt!");
+            DbMethods.getAndFillGrid(dg_versenyzok);
+            resetInputs();
+
+        }
+
+        private void update_btn_Click(object sender, RoutedEventArgs e)
+        {
+            var nev = inp_name.Text;
+            var szuletes = inp_date.SelectedDate;
+            var nem = inp_nem.SelectedItem.ToString();
+            var versenyszamId = DbMethods.getVersenyszamId(inp_verseny.SelectedItem.ToString());
+            var korosztaly = inp_kor.Text;
+
+            if (string.IsNullOrWhiteSpace(nev) || string.IsNullOrWhiteSpace(nem) || string.IsNullOrWhiteSpace(korosztaly) || versenyszamId == 0 || inp_date.SelectedDate is null)
+            {
+                MessageBox.Show("Minden adat kitöltése kötelező!");
+                return;
+            }
+
+            DbMethods.editPerson(activeUserId, nev, szuletes.Value, nem, versenyszamId, korosztaly);
+            MessageBox.Show("A módosítás sikeres volt!");
+            DbMethods.getAndFillGrid(dg_versenyzok);
+            resetInputs();
+
+        }
+
+        private void delete_btn_Click(object sender, RoutedEventArgs e)
+        {
+            if (!dg_versenyzok.SelectedIndex.Equals(-1))
+            {
+                DbMethods.deletePerson(activeUserId);
+                MessageBox.Show("A törlés sikeres volt!");
+                DbMethods.getAndFillGrid(dg_versenyzok);
+                resetInputs();
+
+            }
+
+        }
+
+        private void back_btn_Click(object sender, RoutedEventArgs e)
+        {
+            resetInputs();
+        }
+    }
+}
