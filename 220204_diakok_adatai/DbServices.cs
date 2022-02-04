b769b39305fb2a33a97f09a23bbb970837849072
mrpeterfulop@gmail.com <mrpeterfulop@gmail.com>
2022-02-04 18:39:46 +0100

    02.04. Diákok adatai

---
+++ 220204_diakok_adatai/DbServices.cs
@@ -0,0 +1,204 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace _220204_diakok_adatai
+{
+    class DbServices
+    {
+        public static List<string> GetKepzesList()
+        {
+            var db = new DiakadatDBEntities();
+            return db.Kepzes.Select(x=>x.Nev).ToList();
+        }
+
+        public static List<string> GetOsztalyList()
+        {
+            var db = new DiakadatDBEntities();
+            return db.Tanulo.Select(x => x.Osztaly).Distinct().ToList();
+        }
+
+        public static int GetKepzesId(string kepzes)
+        {
+            var db = new DiakadatDBEntities();
+            return db.Kepzes.FirstOrDefault(x => x.Nev == kepzes).Id;
+        }
+
+        public static void FillDataGrid(DataGrid dg)
+        {
+            var db = new DiakadatDBEntities();
+            dg.ItemsSource =  db.Tanulo.Include("Kepzes").ToList();
+        }
+
+        public static void SearchByStundetId(DataGrid dg, string tanuloId)
+        {
+            var db = new DiakadatDBEntities();
+
+            var res = db.Tanulo.Include("Kepzes")
+                .Where(x=>x.TanuloId.Contains(tanuloId))
+                .ToList();
+            if(res.Count == 0)
+            {
+                MessageBox.Show($"Nincs találat a(z) {tanuloId} azonosítóra!","Nincs találat",MessageBoxButton.OK,MessageBoxImage.Exclamation);
+                return;
+            }
+            dg.ItemsSource = res;
+        }
+
+        public static void SearchByKepzes(DataGrid dg, string kepzes)
+        {
+            var db = new DiakadatDBEntities();
+            var res = db.Tanulo.Include("Kepzes")
+                .Where(x => x.Kepzes.Nev.ToLower().Contains(kepzes.ToLower()))
+                .ToList();
+            if (res.Count == 0)
+            {
+                MessageBox.Show($"Nincs találat a(z) {kepzes} képzésre!", "Nincs találat", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+                return;
+            }
+            dg.ItemsSource = res;
+        }
+
+        public static void CreateTanulo(string tanuloId, string nev, DateTime szuletes, string osztaly, int kepzesId,float atlag)
+        {
+            var db = new DiakadatDBEntities();
+
+            var ujTanulo = new Tanulo()
+            {
+                TanuloId = tanuloId,
+                Nev = nev,
+                Szuletes = szuletes,
+                Osztaly = osztaly,
+                Fk_Kepzes_Id = kepzesId,
+                Atlag = atlag
+            };
+
+            db.Tanulo.Add(ujTanulo);
+            db.SaveChanges();
+            MessageBox.Show("A tanuló hozzáadása sikeres volt!", "Sikeres adatfelvitel", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        public static void UpdateTanulo(int selectedId, string tanuloId, string nev, DateTime szuletes, string osztaly, int kepzesId, float atlag)
+        {
+            var db = new DiakadatDBEntities();
+
+            var edit = db.Tanulo.FirstOrDefault(x => x.Id == selectedId);
+
+            edit.TanuloId = tanuloId;
+            edit.Nev = nev;
+            edit.Szuletes = szuletes;
+            edit.Osztaly = osztaly;
+            edit.Fk_Kepzes_Id= kepzesId;
+            edit.Atlag = atlag;
+
+            db.SaveChanges();
+            MessageBox.Show("A tanuló módosítása sikeres volt!", "Sikeres adatmódosítás", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        public static void DeleteTanulo(int selectedId)
+        {
+            var db = new DiakadatDBEntities();
+            var selected = db.Tanulo.FirstOrDefault(x => x.Id == selectedId);
+            db.Tanulo.Remove(selected);
+            db.SaveChanges();
+            MessageBox.Show("A tanuló törlése sikeres volt!", "Sikeres törlés", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        public static void ExportAllDiak()
+        {
+            var db = new DiakadatDBEntities();
+            var export = db.Tanulo.Include("Kepzes").ToList();
+
+            if(export.Count == 0)
+            {
+                MessageBox.Show("Az adatbázis nem tartalmaz adatokat!", "Hiba! Sikertelen exportálás", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            var dg = new SaveFileDialog();
+            dg.Filter = "CSV fileok (.csv) | .csv";
+            dg.ShowDialog();
+
+            if (dg.FileName == null) return;
+
+            using (var fs= new FileStream(dg.FileName,FileMode.Create))
+            {
+                using (var sw = new StreamWriter(fs,Encoding.UTF8))
+                {
+                    foreach (var e in export)
+                    {
+                        sw.WriteLine($"{e.Nev};{e.Kepzes.Nev}");
+                    }
+                }
+            }
+           MessageBox.Show("A tanulók exportálása sikeres volt!", "Sikeres exportálás", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        public static void ExportAsOsztaly(string osztaly)
+        {
+            var db = new DiakadatDBEntities();
+            var export = db.Tanulo.Include("Kepzes")
+                .Where(x=>x.Osztaly == osztaly)
+                .ToList();
+
+            if (export.Count == 0)
+            {
+                MessageBox.Show($"Az adatbázis nem tartalmaz '{osztaly}' adatokat!", "Hiba! Sikertelen exportálás", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            var dg = new SaveFileDialog();
+            dg.Filter = "CSV fileok (.csv) | .csv";
+            dg.ShowDialog();
+
+            if (dg.FileName == null) return;
+
+            using (var fs = new FileStream(dg.FileName, FileMode.Create))
+            {
+                using (var sw = new StreamWriter(fs, Encoding.UTF8))
+                {
+                    foreach (var e in export)
+                    {
+                        sw.WriteLine($"{e.Osztaly};{e.Nev};{e.Kepzes.Nev}");
+                    }
+                }
+            }
+            MessageBox.Show("A tanulók exportálása sikeres volt!", "Sikeres exportálás", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        public static void ExportAllKepzes()
+        {
+            var db = new DiakadatDBEntities();
+            var export = db.Kepzes.ToList();
+
+            if (export.Count == 0)
+            {
+                MessageBox.Show($"Az adatbázis nem tartalmaz adatokat!", "Hiba! Sikertelen exportálás", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            var dg = new SaveFileDialog();
+            dg.Filter = "CSV fileok (.csv) | .csv";
+            dg.ShowDialog();
+
+            if (dg.FileName == null) return;
+
+            using (var fs = new FileStream(dg.FileName, FileMode.Create))
+            {
+                using (var sw = new StreamWriter(fs, Encoding.UTF8))
+                {
+                    foreach (var e in export)
+                    {
+                        sw.WriteLine($"{e.Nev}");
+                    }
+                }
+            }
+            MessageBox.Show("A képzések exportálása sikeres volt!", "Sikeres exportálás", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+    }
+}
---
+++ 220204_diakok_adatai/MainWindow.xaml.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _220204_diakok_adatai
+{
+    public partial class MainWindow : Window
+    {
+        public int SelectedId { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void FillComboBox(ComboBox cb, List<string> l)
+        {
+            cb.Items.Clear();
+            foreach (var k in l)
+            {
+                cb.Items.Add(k.ToString());
+            }
+            cb.SelectedIndex = 0;
+        }
+
+        private void SetEditOrAdd(bool add=true)
+        {
+            if (add)
+            {
+                add_or_update_label.Content = "Adatok felvitele";
+                form_save_btn.Content = "Hozzáadás";
+                form_delete_btn.Visibility = Visibility.Hidden;
+                form_back_btn.Visibility=Visibility.Hidden;
+            }
+            else
+            {
+                add_or_update_label.Content = "Adatok szerkesztése";
+                form_save_btn.Content = "Módosítás";
+                form_delete_btn.Visibility = Visibility.Visible;
+                form_back_btn.Visibility=Visibility.Visible;
+            }
+        }
+        private void ClearForm()
+        {
+            form_input_id.Clear();
+            form_input_nev.Clear();
+            form_input_szuletes.SelectedDate = DateTime.Now;
+            form_input_osztaly.Clear();
+            form_input_atlag.Clear();
+        }
+
+        private void UpdateScreen()
+        {
+            DbServices.FillDataGrid(dg_diakok);
+            ClearForm();
+            SetEditOrAdd();
+            FillComboBox(export_combo_osztaly, DbServices.GetOsztalyList());
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            FillComboBox(export_combo_osztaly,DbServices.GetOsztalyList());
+            FillComboBox(form_input_kepzes,DbServices.GetKepzesList());
+            SetEditOrAdd();
+            DbServices.FillDataGrid(dg_diakok);
+        }
+
+        private void search_btn_submit_Click(object sender, RoutedEventArgs e)
+        {
+            if(search_radio_kepzes.IsChecked == true)
+            {
+                DbServices.SearchByKepzes(dg_diakok, search_input.Text);
+            }
+            else
+            {
+                DbServices.SearchByStundetId(dg_diakok, search_input.Text);
+            }
+
+        }
+
+        private void search_btn_reset_Click(object sender, RoutedEventArgs e)
+        {
+            DbServices.FillDataGrid(dg_diakok);
+            search_input.Clear();
+            search_radio_kepzes.IsChecked = true;
+        }
+
+        private void dg_diakok_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+
+            var s = (Tanulo)dg_diakok.SelectedItem;
+
+            if(s != null)
+            {
+                SetEditOrAdd(false);
+
+                SelectedId = s.Id;
+                form_input_id.Text = s.TanuloId;
+                form_input_nev.Text = s.Nev;
+                form_input_szuletes.SelectedDate = s.Szuletes;
+                form_input_osztaly.Text = s.Osztaly;
+                form_input_kepzes.Text = s.Kepzes.Nev;
+                form_input_atlag.Text = s.Atlag.ToString("0.0");
+            }
+        }
+
+        private void form_save_btn_Click(object sender, RoutedEventArgs e)
+        {
+            var tanuloId = form_input_id.Text.Trim();
+            var nev = form_input_nev.Text.Trim();
+            var szuletes = form_input_szuletes;
+            var osztaly = form_input_osztaly.Text.Trim();
+            var kepzes = form_input_kepzes.Text.Trim();
+            var atlag = form_input_atlag.Text.Trim().Replace('.', ',');
+
+
+            if (string.IsNullOrWhiteSpace(tanuloId) ||
+                string.IsNullOrWhiteSpace(nev) ||
+                string.IsNullOrWhiteSpace(osztaly) ||
+                string.IsNullOrWhiteSpace(kepzes) ||
+                string.IsNullOrWhiteSpace(atlag) ||
+                szuletes.SelectedDate is null)
+            {
+                MessageBox.Show("Minden mező kitöltése kötelező!", "Hiba!", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            if (!float.TryParse(atlag, out float x) || float.Parse(atlag) < 1 || float.Parse(atlag) > 5)
+            {
+                MessageBox.Show("Az átlag mező értéke csak 1 és 5 közé eső szám lehet!", "Hiba a formátumban!", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            if(tanuloId.Length <10 || tanuloId.Length > 10)
+            {
+                MessageBox.Show("A tanuló azonosítója egy 10 karaterből álló számsor!", "Hiba a formátumban!", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            if (SelectedId > 0)
+            {
+                var res = MessageBox.Show("Biztosan módosítani szeretnéd az adatokat?", "Megerősítési kérelem!", MessageBoxButton.YesNo, MessageBoxImage.Question);
+                if(res == MessageBoxResult.No)
+                {
+                    return;
+                }
+
+                DbServices.UpdateTanulo(SelectedId,
+                    tanuloId,
+                    nev,
+                    szuletes.SelectedDate.Value,
+                    osztaly,
+                    DbServices.GetKepzesId(kepzes),
+                    float.Parse(atlag));
+            }
+            else
+            {
+                DbServices.CreateTanulo(tanuloId,
+                    nev,
+                    szuletes.SelectedDate.Value,
+                    osztaly,
+                    DbServices.GetKepzesId(kepzes),
+                    float.Parse(atlag));
+            }
+
+            UpdateScreen();
+        }
+
+        private void form_delete_btn_Click(object sender, RoutedEventArgs e)
+        {
+            var res = MessageBox.Show("Biztosan törölni szeretnéd a tételt?", "Megerősítési kérelem!", MessageBoxButton.YesNo, MessageBoxImage.Question);
+            if (res == MessageBoxResult.No)
+            {
+                return;
+            }
+            DbServices.DeleteTanulo(SelectedId);
+            UpdateScreen();
+        }
+
+        private void form_back_btn_Click(object sender, RoutedEventArgs e)
+        {
+            ClearForm();
+            SetEditOrAdd();
+            SelectedId = 0;
+        }
+
+        private void export_radio_osztaly_Click(object sender, RoutedEventArgs e)
+        {
+            export_combo_osztaly.IsEnabled = true;
+        }
+
+        private void export_radio_diak_Click(object sender, RoutedEventArgs e)
+        {
+            export_combo_osztaly.IsEnabled = false;
+        }
+
+        private void export_radio_kepzes_Click(object sender, RoutedEventArgs e)
+        {
+            export_combo_osztaly.IsEnabled = false;
+        }
+
+        private void export_btn_submit_Click(object sender, RoutedEventArgs e)
+        {
+            if(export_radio_diak.IsChecked == true)
+            {
+                DbServices.ExportAllDiak();
+            }else if(export_radio_kepzes.IsChecked == true)
+            {
+                DbServices.ExportAllKepzes();
+            }
+            else if(export_radio_osztaly.IsChecked == true && export_combo_osztaly.Text.Length > 0)
+            {
+                DbServices.ExportAsOsztaly(export_combo_osztaly.Text.ToString());
+            }
+        }
+
+
+    }
+}
